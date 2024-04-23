ff2521cf8684d5c907c23bf8df71f03763248b76
cherukuribh <cherukuri.barathi@gmail.com>
2024-04-23 16:01:21 +0100

    made some changes, added print

--- DFdemo.py
+++ DFdemo.py
@@ -20,4 +20,5 @@ if __name__ == '__main__':
     #ordersdf = spark.read.option("header","true").schema(orderSchema).csv("C:\\Users\\cheru\\PycharmProjects\\pythonProject1\\orders.csv")
     ordersdf = spark.read.option("header","true").schema(orderSchema).csv("/user/ec2-user/UKUSMarHDFS/bharathi/orders.csv")
     print("testing webhook")
+    print("testing 2 webhook")
     ordersdf.show(5)
